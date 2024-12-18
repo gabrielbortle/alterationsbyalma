05c33a5ddf914fa6a2898c3bfee58b46537db955
Gabriel <gabrielbortle@gmail.com>
2024-12-18 12:26:46 -0700

    env

--- src/shop/services/woocommerce.js
+++ src/shop/services/woocommerce.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_URL = 'https://alterationsbyalma.com/wp-json/wc/v3/products';
-const CONSUMER_KEY = 'ck_72f8ab228e8b75d535ab255f5efc6edfb2cd26ba';
-const CONSUMER_SECRET = 'cs_62059f9bdd32c7bca62d1e2d111ce32a4bd17062';
+const CONSUMER_KEY = import.meta.env.VITE_WOOCOMMERCE_CONSUMER_KEY;
+const CONSUMER_SECRET = import.meta.env.VITE_WOOCOMMERCE_CONSUMER_SECRET;
 
 export const fetchProducts = async () => {
   try {
