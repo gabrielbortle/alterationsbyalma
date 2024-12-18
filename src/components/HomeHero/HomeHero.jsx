7c1ff37d14c7261a5cf5060b3ee87edb6ee2002e
Gabriel <gabrielbortle@gmail.com>
2024-12-18 16:55:09 -0700

    changed a tag to link

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HomeHero.css';
 import background from './background.jpg'; // Import the image
 
@@ -13,8 +14,8 @@ const HomeHero = () => {
           <h1>Alterations By Alma</h1>
           <p>Perfect Fit, Every Time</p>
           <div className="hero-buttons">
-            <a href="/contact" className="btn-primary">Book an Appointment</a>
-            <a href="/services" className="btn-secondary">Learn More</a>
+            <Link to="/contact" className="btn-primary">Book an Appointment</Link>
+            <Link to="/services" className="btn-secondary">Learn More</Link>
           </div>
         </div>
       </div>
