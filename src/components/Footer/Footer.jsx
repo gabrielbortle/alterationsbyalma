2ab97abee35c8588ae26b1049ebef8007fac3094
Gabriel <gabrielbortle@gmail.com>
2025-05-06 19:23:32 -0600

    address update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
         <div className="grid-container">
           <div className="info">
             <h3>Location</h3>
-            <p>30 W Birch Circle</p>
-            <p>Victor ID, 83455</p>
+            <p>232 W 3175 S</p>
+            <p>Nibley, UT 84321</p> <br />
             <p>*Appointment Only*</p>
-            <p>*DropBox Available*</p>
+            <p>*DropBox Available*</p> <br />
             <h2>Make an appointment!</h2>
             <h3>Alma Giron</h3>
             <p>(817) 793 - 2008</p>
